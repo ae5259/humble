5d74ca776f725221ee5f1cf6d7fafca1b8db2d87
ae5259 <isakulovdev@gmail.com>
2025-06-11 18:42:58 +0500

    fix: meta tags

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,26 +1,23 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 
-function SEO({
-  title = "akumarujon UwU",
-  description =
-    "Konichiwa, I'm akumarujon, self-taught Developer from Uzbekistan. This is my personal website. You can find more information about me here.",
-  url = "https://akumaru.ninja",
-  image = "https://akumaru.ninja/og.png",
-}) {
+function SEO() {
   return (
     <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta
+        name="description"
+        content="Konichiwa, I'm akumarujon, self-taught Developer from Uzbekistan. This is my personal website. You can find more information about me here."
+      />
+      <meta
+        property="og:description"
+        content="Konichiwa, I'm akumarujon, self-taught Developer from Uzbekistan. This is my personal website. You can find more information about me here."
+      />
       <meta property="og:site_name" content="akumarujon UwU" />
-      <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
+      <meta property="og:url" content="https://akumaru.ninja" />
+      <meta property="og:image" content="https://akumaru.ninja/og.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content="https://akumaru.ninja/og.png" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
+      <title>akumarujon UwU</title>
     </Helmet>
   );
 }
